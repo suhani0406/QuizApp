b894eecbcbdecf7d1553e3a2ec15bdea1744845e
suhani0406 <suhanisinha31558@gmail.com>
2025-01-06 15:41:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Quizapp from './Quizapp'
+export default function App() {
+  return (
+    <div>
+      <Quizapp />
+    </div>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+      Question: 'What is Redux?',
+      Option1: 'Redux is State Management System',
+      Option2: 'Redux is State',
+      Option3: 'Redux is Data Type',
+      Option4: 'Redux is a part of HTML',
+    },
+    {
+      Question: 'What does HTML stand for?',
+      Option1: 'HyperText Markup Language',
+      Option2: 'HighText Machine Language',
+      Option3: 'HyperTool Markup Language',
+      Option4: 'None of the above',
+    },
+    {
+      Question: 'Which of the following is a JavaScript framework?',
+      Option1: 'React',
+      Option2: 'Laravel',
+      Option3: 'Django',
+      Option4: 'Ruby on Rails',
+    },
+    {
+      Question: 'What is the purpose of CSS?',
+      Option1: 'To add styling to HTML elements',
+      Option2: 'To structure web content',
+      Option3: 'To interact with the server',
+      Option4: 'To manage database connections',
+    },
+    {
+      Question: 'Which of the following is not a programming language?',
+      Option1: 'HTML',
+      Option2: 'Python',
+      Option3: 'Java',
+      Option4: 'C++',
+    },
+    {
+      Question: 'What does SQL stand for?',
+      Option1: 'Structured Query Language',
+      Option2: 'Sequential Query Language',
+      Option3: 'Standard Query Link',
+      Option4: 'Simple Query Language',
+    },
+  ];
+  
